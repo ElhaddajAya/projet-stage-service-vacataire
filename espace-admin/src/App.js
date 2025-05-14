6c21825bb7aea5c433b00c89494bd42ae6898e05
ElhaddajAya <ayaelhaddaj619@gmail.com>
2025-05-14 16:31:17 +0100

    Created VacataireList drafts in espace-admin

--- espace-admin/src/App.js
+++ espace-admin/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import VacataireList from './components/VacataireList';
 
 function App() {
   return (
     <div>
-    
+    <VacataireList />
     </div>
   );
 }
