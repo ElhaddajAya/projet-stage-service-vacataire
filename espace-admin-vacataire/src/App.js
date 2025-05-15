49030ff71364b964ba1cfb2ad026eef6e529b5eb
ElhaddajAya <ayaelhaddaj619@gmail.com>
2025-05-15 01:09:11 +0100

    LoginForm + Header CSS created

--- espace-admin-vacataire/src/App.js
+++ espace-admin-vacataire/src/App.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginForm from './components/LoginForm';
+import Header from './espace-vacataire/components/Header';
 
 function App() {
- 
+    return (
+    <div>
+        {/* <Header /> */}
+        <LoginForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ espace-admin-vacataire/src/espace-vacataire/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../../src/logo.jpg'; // Assure-toi que le logo est dans ton dossier src
+import "../../styles/global.css"; // Assure-toi que le fichier CSS est dans le bon chemin
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <img src={logo} alt="Logo" />
+      </div>
+      <h1>Espace Enseignant Vacataire</h1>
+    </header>
+  );
+};
+
+export default Header;
