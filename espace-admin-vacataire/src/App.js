6a7e6c6d11dddc0364a70462a28e901abdd7e9ce
ElhaddajAya <ayaelhaddaj619@gmail.com>
2025-05-15 02:12:58 +0100

    VacataireList created

--- espace-admin-vacataire/src/App.js
+++ espace-admin-vacataire/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import LoginForm from './components/LoginForm';
+import VacataireList from './espace-admin/pages/VacataireList';
 import Header from './espace-vacataire/components/Header';
 
 function App() {
     return (
     <div>
         {/* <Header /> */}
-        <LoginForm />
+        {/* <LoginForm /> */}
+        <VacataireList />
     </div>
   );
 }
