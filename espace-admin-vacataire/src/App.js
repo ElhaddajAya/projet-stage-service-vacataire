ddd5327f13c3dc6ed4454362ee77561ee3983fc7
ElhaddajAya <ayaelhaddaj619@gmail.com>
2025-05-17 02:32:31 +0100

    Added custom fonts

--- espace-admin-vacataire/src/App.js
+++ espace-admin-vacataire/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import LoginForm from './components/LoginForm';
 import VacataireList from './espace-admin/pages/VacataireList';
 import SuiviDossier from './espace-vacataire/pages/SuiviDossier';
+import './styles/global.css'
 
 function App() {
     return (
